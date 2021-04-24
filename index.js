c6926a8335d98079497fbb48291c3aaa52a6a70e
stkirk <shaanekirk@gmail.com>
2021-04-24 12:14:52 -0400

    task 2 completed

--- index.js
+++ index.js
@@ -47,7 +47,8 @@ Do the following:
 
    HINT: look up the Number method
 */
-
+let stringVar = Number("1999");
+console.log(stringVar);
 /*
 Task 1d - Multiply
  
@@ -71,8 +72,8 @@ Do the following:
    3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/) {
-  /*add your code here*/
+function dogYears(age) {
+  return age * 7;
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
